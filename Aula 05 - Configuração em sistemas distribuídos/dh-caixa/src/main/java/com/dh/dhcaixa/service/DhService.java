0edd621a55dc881e3ff620de4bb3bd1d0db359e3
Aline Pollis <aline.pollis@hotmail.com>
2022-07-02 02:47:35 -0300

    Aula 05 - Configuração em sistemas distribuídos

---
+++ Aula 05 - Configuração em sistemas distribuídos/dh-caixa/src/main/java/com/dh/dhcaixa/service/DhService.java
@@ -0,0 +1,7 @@
+package com.dh.dhcaixa.service;
+
+public interface DhService {
+    void setQtdeNotas(String aux);
+
+    String getSaldo();
+}
---
+++ Aula 05 - Configuração em sistemas distribuídos/dh-caixa/src/test/java/com/dh/dhcaixa/DhCaixaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.dhcaixa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhCaixaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

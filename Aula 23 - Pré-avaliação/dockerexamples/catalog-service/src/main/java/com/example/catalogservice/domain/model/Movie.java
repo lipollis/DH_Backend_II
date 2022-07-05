cb82f58304d005d8b67de74f4c0576c834385e6d
Aline Pollis <aline.pollis@hotmail.com>
2022-07-05 20:59:48 -0300

    Aula 19 - Log Aggregation

---
+++ Aula 23 - Pré-avaliação/dockerexamples/catalog-service/src/main/java/com/example/catalogservice/domain/model/Movie.java
@@ -0,0 +1,19 @@
+package com.example.catalogservice.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+
+	private Long id;
+
+	private String name;
+
+	private String genre;
+
+	private String urlStream;
+}
